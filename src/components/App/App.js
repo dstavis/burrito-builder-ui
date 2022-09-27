6bfa328e1000c5da58bcc521553d9a1e7b88dc31
David Stavis <davidstavis@gmail.com>
2022-09-27 09:20:14 -0600

    Iteration 1 complete: fixed state bug

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,10 @@ import OrderForm from '../../components/OrderForm/OrderForm';
 class App extends Component {
   constructor(props) {
     super();
+
+    this.state = {
+      orders: []
+    }
   }
 
   componentDidMount() {
